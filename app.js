09aa3bfb107aeb162b941354075e08adc3c3fd55
Brian McGrath <Brian.McGrath@otised.com>
2018-10-14 03:40:25 -0400

    added env

--- app.js
+++ app.js
@@ -1,22 +1,30 @@
 var express = require('express');
+require('dotenv').config();
 var app = express();
 var fs = require('fs');
 var http = require('http');
 var https = require('https');
-var sslOptions = {
-    key: fs.readFileSync('/root/ssl/BrianScience.key'),   
-    cert: fs.readFileSync('/root/ssl/Brian_Science.crt'),   
-    ca: fs.readFileSync ('/root/ssl/Brian_Science.ca-bundle')   
-   };
+
 
 app.get('/',function(req,res){
     res.send('<h1>David is a hoe</h1>')
 });
 
 //creates the server+redirect from http to https.
-http.createServer(function (req, res) {
-    res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
-    res.end();
-}).listen(80);
-https.createServer(sslOptions, app).listen(443)
+
+if (process.env.ENV == 'production'){
+    var sslOptions = {
+        key: fs.readFileSync('/root/ssl/BrianScience.key'),   
+        cert: fs.readFileSync('/root/ssl/Brian_Science.crt'),   
+        ca: fs.readFileSync ('/root/ssl/Brian_Science.ca-bundle')   
+    };    
+    https.createServer(sslOptions, app).listen(443)
+    http.createServer(function (req, res) {
+        res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
+        res.end();
+    }).listen(80);
+} else {
+    http.createServer(app).listen(80);
+}
+
 
